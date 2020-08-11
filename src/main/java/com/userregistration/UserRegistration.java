8de3aec6f09d666f517665ccde2e6334f7a6b308
sujit gargate <sujitgargate5893@gmail.com>
2020-08-11 22:46:19 +0530

    Refactored : For Email Validation

--- src/main/java/com/userregistration/UserRegistration.java
+++ src/main/java/com/userregistration/UserRegistration.java
@@ -11,19 +11,20 @@ public class UserRegistration {
         return(firstName.matches("[A-Z][a-z]{3,}"));
     }
 
-    //This method is for checking Valid first name
+    //This method is for checking Valid last name
     public boolean checkLastName(String lastName) {
 
         return(lastName.matches("[A-Z][a-z]{3,}"));
     }
 
-    //
+    //This method is for checking Valid Email Address
     public boolean checkUserEmail(String userEmail) {
 
         String pattern = "^[a-zA-Z]+([._+-][a-zA-Z]+)*[@][A-Za-z]+[.][a-zA-Z]+([.][a-zA-Z]{2})*$";
         return (userEmail.matches(pattern));
     }
 
+    //This method is for checking Valid Phone Number
     public boolean checkUserPhoneNumber(String userPhoneNumber) {
         String pattern = "^[0-9]{1,2}[ ][0-9]{10}$";
         return (userPhoneNumber.matches(pattern));
