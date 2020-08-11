8153ab63ed6410796493b3404646bfa5492d0bae
sujit gargate <sujitgargate5893@gmail.com>
2020-08-11 20:42:10 +0530

    Refactored : For Checking last name

--- src/main/java/com/userregistration/UserRegistration.java
+++ src/main/java/com/userregistration/UserRegistration.java
@@ -1,27 +1,19 @@
 package com.userregistration;
 
-import java.util.Scanner;
-
 public class UserRegistration {
-    Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        UserRegistration obj = new UserRegistration();
-        if(obj.checkFirstName() == true) {
-            System.out.println("Entered name is in correct format");
-        } else {
-            System.out.println("Enter valid name");
-        }
     }
 
-    public boolean checkFirstName() {
-        System.out.println("Enter the name");
-        String firstName = sc.next();
-        if(firstName.matches( "[A-Z][a-z]*" ) == true){
-            return true;
-        }
-        else {
-            return false;
-        }
+    //This method is for checking Valid first name
+    public boolean checkFirstName(String firstName) {
+
+        return(firstName.matches("[A-Z][a-z]{3,}"));
+    }
+
+    //This method is for checking Valid first name
+    public boolean checkLastName(String lastName) {
+
+        return(lastName.matches("[A-Z][a-z]{3,}"));
     }
 }
--- src/test/java/com/userregistration/UserRegistrationTest.java
+++ src/test/java/com/userregistration/UserRegistrationTest.java
@@ -3,13 +3,25 @@ package com.userregistration;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Scanner;
+
 public class UserRegistrationTest {
+
+    UserRegistration userregistration = new UserRegistration();
+
+    //This case is for checking Valid first name
     @Test
     public void enteredFirstName_Should_Return_True() {
 
-        UserRegistration userregistration = new UserRegistration();
+        String firstName = "Sujit";
+        Assert.assertTrue("InValid FirstName", userregistration.checkFirstName(firstName));
+    }
 
-        boolean nameValidation = userregistration.checkFirstName();
-        Assert.assertSame("True",nameValidation);
+    //This case is for checking Valid last name
+    @Test
+    public void enteredLastName_Should_Return_True() {
+
+        String lastName = "Gargate";
+        Assert.assertTrue("InValid LastName", userregistration.checkLastName(lastName));
     }
 }
